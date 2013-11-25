e52d497fd8f7893563b563c915121af1bcb5ad08
gorkar12 <gorkar12@gmail.com>
2013-11-25 20:05:26 +0100

    DB username changed

--- server/settings.py
+++ server/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'taxiexpress',                      # Or path to database file if using sqlite3.
-        'USER': 'postgress',                      # Not used with sqlite3.
+        'USER': 'taxiexpress',                      # Not used with sqlite3.
         'PASSWORD': '1111',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
