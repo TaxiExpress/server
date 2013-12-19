11f568cd1381d626da5c878e8000027affe76233
gorkar12 <gorkar12@gmail.com>
2013-12-19 13:50:13 +0100

    logfile removed

--- server/settings.py
+++ server/settings.py
@@ -157,10 +157,10 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': '/var/log/django/myapp.log'
-        }
+        # 'logfile': {
+        #     'class': 'logging.handlers.WatchedFileHandler',
+        #     'filename': '/var/log/django/myapp.log'
+        # }
     },
     'loggers': {
         'django.request': {
