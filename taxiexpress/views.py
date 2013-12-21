ca3f4e064e324134be0ec0bbf4156f0fd3f6dd4e
Mikeldeltio9 <Mikeldeltio90@gmail.com>
2013-12-21 19:40:26 +0100

    SMS Send Added

--- taxiexpress/views.py
+++ taxiexpress/views.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from django import forms
+from nexmomessage import NexmoMessage
 from django.core.mail import EmailMessage
 from django.core.mail import EmailMultiAlternatives
 from django.forms import CharField,Form,PasswordInput
@@ -76,13 +77,28 @@ def registerUser(request):
         #   return HttpResponse("shortpassword", content_type="text/plain")
         else:
             try:
-                validate_email( request.POST['email'] )
-                c = Customer(email=request.POST['email'], password=passtemp, phone=request.POST['phone'], lastUpdate=datetime.strptime(request.POST['lastUpdate'], '%Y-%m-%d %H:%M:%S'))
+            	c = Customer(username=request.POST['email'], password=passtemp, phone=request.POST['phone'],'%Y-%m-%d %H:%M:%S'))
                 c.save()
                 code = random.randint(1, 999999)
                 c.validationCode = code
                 c.save()
-                #enviar sms de forma mágica con el código
+            	
+            	msg = {
+					    'reqtype': 'json',
+					    'api_key': '8a352457',
+					    'api_secret': '460e58ff',
+					    'from': '619317759',
+					    'to': equest.POST['phone'],
+					    'text': 'Su codigo de validacion de Taxi Express es: ' + code
+					}
+				
+				sms = NexmoMessage(msg)
+				sms.set_text_info(msg['text'])
+
+				response = sms.send_request()
+
+				# Falta saber si devuelve un JSON o un XML, depende de la API a la que haga la POST la libreria.
+
                 
                 return HttpResponse(status=201,content="Creado")
             except ValidationError:
@@ -90,10 +106,6 @@ def registerUser(request):
     else:
         return HttpResponseBadRequest()
 
-def testEmail(request):
-    email = EmailMessage('Hello', 'World', to=['gorka_12@hotmail.com'])
-    email.send()
-    return HttpResponse(status=200,content="OK")
 
 def getClosestTaxi(request):
     if request.GET.get('latitud', "false") != "false":
@@ -107,62 +119,6 @@ def test(request):
     return HttpResponse(status=201,content=lista)
 
         #return JSON with taxi info
-
-def loadData(request):
-    co = Country(code = 108,name='Espana')
-    co.save()
-    s = State(code = 48, name = 'Bizkaia', country = co)
-    s.save()
-    ci = City(code = 013, name = 'Barakaldo', state = s)
-    ci.save()
-    ci = City(code = 015, name = 'Basauri', state = s)
-    ci.save()
-    ci = City(code = 016, name = 'Berango', state = s)
-    ci.save()
-    ci = City(code = 017, name = 'Bermeo', state = s)
-    ci.save()
-    ci = City(code = 020, name = 'Bilbao', state = s)
-    ci.save()
-    ci = City(code = 027, name = 'Durango', state = s)
-    ci.save()
-    ci = City(code = 036, name = 'Galdakao', state = s)
-    ci.save()
-    ci = City(code = 040, name = 'Gatika', state = s)
-    ci.save()
-    ci = City(code = 043, name = 'Gorliz', state = s)
-    ci.save()
-    ci = City(code = 044, name = 'Getxo', state = s)
-    ci.save()
-    ci = City(code = 046, name = 'Gernika-Lumo', state = s)
-    ci.save()
-    ci = City(code = 054, name = 'Leioa', state = s)
-    ci.save()
-    ci = City(code = 057, name = 'Lekeitio', state = s)
-    ci.save()
-    ci = City(code = 065, name = 'Ugao-Miraballes', state = s)
-    ci.save()
-    ci = City(code = 78, name = 'Portugalete', state = s)
-    ci.save()
-    ci = City(code = 81, name = 'Lezama', state = s)
-    ci.save()
-    ci = City(code = 82, name = 'Santurtzi', state = s)
-    ci.save()
-    ci = City(code = 84, name = 'Sestao', state = s)
-    ci.save()
-    ci = City(code = 85, name = 'Sopelana', state = s)
-    ci.save()
-    ci = City(code = 89, name = 'Urduliz', state = s)
-    ci.save()
-    dr = Driver(email="conductor@gmail.com", password="1111", phone="656111112", first_name="Conductor", last_name="DePrimera", city=ci)
-    dr.save()
-    dr2 = Driver(email="conductor2@gmail.com", password="1111", phone="656111113", first_name="Conductor", last_name="DeSegunda", city=ci)
-    dr2.save()
-    cu = Customer(email="gorka_12@hotmail.com", password="1111", phone="656111111", first_name="Pepito", last_name="Palotes", city=ci, lastUpdate=datetime.strptime('1980-01-01 00:00:01','%Y-%m-%d %H:%M:%S'))
-    cu.save()
-    cu.favlist.add(dr)
-    cu.favlist.add(dr2)
-    return HttpResponse(status=201,content="Cargado")
-    
 
 def validateUser(request):
     #IMPORTANTE, el contenido del email no es correcto, hay que actualizarlo.
@@ -197,10 +153,10 @@ def changePassword(request):
         if customer.password == request.POST['oldPass']:
             customer.password = request.POST['newPass']  
             customer.save()
-            subject = 'Parking Express: Tu contraseña ha sido modificada'
+            subject = 'Taxi Express: Tu contraseña ha sido modificada'
             from_email = 'MyTaxiExpress@gmail.com'
             to = [customer.email]
-            html_content = 'Bienvenido a Taxi Express! <br> <br> Para comenzar a utilizar nuestros servicios deberás confirmar tu dirección de correo eletrónico haciendo click en el siguiente enlace: <br> <br> <a href="https://manage.stripe.com/confirm_email?t=z5roGRDbZdRbvknLfTZHCUSCyvPeznIw"> <br> <br> Un saludo de parte del equipo de Taxi Express.'
+            html_content = 'Le informamos de que su contraseña de Taxi Express ha sido modificada. En el caso en el que no tenga constancia de ello, pongase inmediantamente en contactocon MyTaxiExpress@gmail.com.'
             msg = EmailMessage(subject, html_content, from_email, to)
             msg.content_subtype = "html"  # Main content is now text/html
             msg.send()  
@@ -259,3 +215,59 @@ def removeFavoriteDriver(request):
         return HttpResponse(status=201,content="Taxista eliminado de la lista de favoritos")
     else:
         return HttpResponseBadRequest()
+
+
+def loadData(request):
+    co = Country(code = 108,name='Espana')
+    co.save()
+    s = State(code = 48, name = 'Bizkaia', country = co)
+    s.save()
+    ci = City(code = 013, name = 'Barakaldo', state = s)
+    ci.save()
+    ci = City(code = 015, name = 'Basauri', state = s)
+    ci.save()
+    ci = City(code = 016, name = 'Berango', state = s)
+    ci.save()
+    ci = City(code = 017, name = 'Bermeo', state = s)
+    ci.save()
+    ci = City(code = 020, name = 'Bilbao', state = s)
+    ci.save()
+    ci = City(code = 027, name = 'Durango', state = s)
+    ci.save()
+    ci = City(code = 036, name = 'Galdakao', state = s)
+    ci.save()
+    ci = City(code = 040, name = 'Gatika', state = s)
+    ci.save()
+    ci = City(code = 043, name = 'Gorliz', state = s)
+    ci.save()
+    ci = City(code = 044, name = 'Getxo', state = s)
+    ci.save()
+    ci = City(code = 046, name = 'Gernika-Lumo', state = s)
+    ci.save()
+    ci = City(code = 054, name = 'Leioa', state = s)
+    ci.save()
+    ci = City(code = 057, name = 'Lekeitio', state = s)
+    ci.save()
+    ci = City(code = 065, name = 'Ugao-Miraballes', state = s)
+    ci.save()
+    ci = City(code = 78, name = 'Portugalete', state = s)
+    ci.save()
+    ci = City(code = 81, name = 'Lezama', state = s)
+    ci.save()
+    ci = City(code = 82, name = 'Santurtzi', state = s)
+    ci.save()
+    ci = City(code = 84, name = 'Sestao', state = s)
+    ci.save()
+    ci = City(code = 85, name = 'Sopelana', state = s)
+    ci.save()
+    ci = City(code = 89, name = 'Urduliz', state = s)
+    ci.save()
+    dr = Driver(email="conductor@gmail.com", password="1111", phone="656111112", first_name="Conductor", last_name="DePrimera", city=ci)
+    dr.save()
+    dr2 = Driver(email="conductor2@gmail.com", password="1111", phone="656111113", first_name="Conductor", last_name="DeSegunda", city=ci)
+    dr2.save()
+    cu = Customer(email="gorka_12@hotmail.com", password="1111", phone="656111111", first_name="Pepito", last_name="Palotes", city=ci, lastUpdate=datetime.strptime('1980-01-01 00:00:01','%Y-%m-%d %H:%M:%S'))
+    cu.save()
+    cu.favlist.add(dr)
+    cu.favlist.add(dr2)
+    return HttpResponse(status=201,content="Cargado")
