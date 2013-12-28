41c981a6046ae3af7e6c2f8663a8f4f18da45532
gorkar12 <gorkar12@gmail.com>
2013-12-28 17:42:31 +0100

    Added travel history to login response

--- taxiexpress/serializers.py
+++ taxiexpress/serializers.py
@@ -7,8 +7,15 @@ class DriverSerializer(serializers.ModelSerializer):
         model = Driver
         fields = ('email', 'phone', 'first_name', 'last_name', 'image')
 
+class TravelSerializer(serializers.ModelSerializer):
+    driver= DriverSerializer()
+    class Meta:
+        model = Travel
+        fields = ('driver', 'starttime', 'endtime', 'cost', 'startpoint', 'origin', 'endpoint', 'destination')
+
 class CustomerSerializer(serializers.ModelSerializer):
     favlist = DriverSerializer(many=True)
+    travel_set = TravelSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('email', 'phone', 'first_name', 'last_name', 'lastUpdate', 'favlist')
+        fields = ('email', 'phone', 'first_name', 'last_name', 'lastUpdate', 'favlist', 'travel_set')
