c62d7334a5c0406b2c2464893dc10a8f9c2c5c2d
MikelDTB <mikeldeltio90@gmail.com>
2013-12-29 16:42:49 +0100

    Email account updated.

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = []
 #Email 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'MyTaxiExpress@gmail.com'
-EMAIL_HOST_PASSWORD = 'carlosmoro'
+EMAIL_HOST_PASSWORD = 'Carlosmoro'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
