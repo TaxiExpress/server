aaa34f914b036b9e1e79123c7ed621aeb0eda76e
David Lallana <davidlallana@gmail.com>
2014-01-05 18:47:06 +0100

    Updated Driver Serializer
    - Added Geom

--- taxiexpress/serializers.py
+++ taxiexpress/serializers.py
@@ -15,7 +15,7 @@ class DriverSerializer(serializers.ModelSerializer):
             return int(5*obj.positiveVotes/(obj.positiveVotes+obj.negativeVotes))
     class Meta:
         model = Driver
-        fields = ('email', 'phone', 'first_name', 'last_name', 'image', 'valuation', 'car')
+        fields = ('email', 'phone', 'first_name', 'last_name', 'image', 'valuation', 'car', 'geom')
 
 class TravelSerializer(serializers.ModelSerializer):
     driver= DriverSerializer()
