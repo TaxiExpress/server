e8e83b81905729d744b4d406a2cb1210358a2eda
gorkar12 <gorkar12@gmail.com>
2014-01-19 12:43:27 +0100

    Returning sessionID in all serializers

--- taxiexpress/serializers.py
+++ taxiexpress/serializers.py
@@ -32,7 +32,7 @@ class DriverSerializer(serializers.ModelSerializer):
                 return int(5*obj.positiveVotes/(obj.positiveVotes+obj.negativeVotes))
     class Meta:
         model = Driver
-        fields = ('email', 'phone', 'first_name', 'last_name', 'image', 'valuation', 'car', 'geom')
+        fields = ('email', 'phone', 'sessionID', 'first_name', 'last_name', 'image', 'valuation', 'car', 'geom')
 
 class TravelSerializer(serializers.ModelSerializer):
     driver= DriverSerializer()
@@ -47,7 +47,7 @@ class CustomerCompleteSerializer(serializers.ModelSerializer):
     travel_set = TravelSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateFavorites','lastUpdateTravels', 'favlist', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity', 'travel_set')
+        fields = ('email', 'phone', 'sessionID', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateFavorites','lastUpdateTravels', 'favlist', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity', 'travel_set')
 
 #Este serializer devuelve la lista de taxistas favoritos + lista de viajes
 class CustomerTaxiesTravelsSerializer(serializers.ModelSerializer):
@@ -55,38 +55,38 @@ class CustomerTaxiesTravelsSerializer(serializers.ModelSerializer):
     travel_set = TravelSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('favlist', 'travel_set','lastUpdateFavorites','lastUpdateTravels')
+        fields = ('sessionID', 'favlist', 'travel_set','lastUpdateFavorites','lastUpdateTravels')
 
 #Este serializer devuelve la lista de viajes
 class CustomerTravelsSerializer(serializers.ModelSerializer):
     travel_set = TravelSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('travel_set','lastUpdateTravels')
+        fields = ('sessionID', 'travel_set','lastUpdateTravels')
 
 #Este serializer devuelve solo el perfil del Customer
 class CustomerProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
+        fields = ('sessionID', 'email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
 
 #Este serializer devuelve el perfil del Customer + lista de taxistas
 class CustomerProfileTaxiesSerializer(serializers.ModelSerializer):
     favlist = DriverSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('favlist','email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateFavorites', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
+        fields = ('sessionID', 'favlist','email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateFavorites', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
 
 #Este serializer devuelve el perfil del Customer + lista de viajes
 class CustomerProfileTravelsSerializer(serializers.ModelSerializer):
     travel_set = TravelSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('travel_set','email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateTravels', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
+        fields = ('sessionID', 'travel_set','email', 'phone', 'first_name', 'last_name', 'image', 'lastUpdate','lastUpdateTravels', 'fAccessible', 'fAnimals', 'fAppPayment', 'fCapacity')
 
 #Este serializer devuelve solo la lista de taxistas favoritos
 class CustomerTaxiesSerializer(serializers.ModelSerializer):
     favlist = DriverSerializer(many=True)
     class Meta:
         model = Customer
-        fields = ('favlist','lastUpdateFavorites')
+        fields = ('sessionID', 'favlist','lastUpdateFavorites')
