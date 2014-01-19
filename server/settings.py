2fc6c36b0e2104063065bfca0ffde42f24286199
Laura Sainz de murieta <laura.sdml@gmail.com>
2014-01-19 21:33:55 +0100

    show request values when errors

--- server/settings.py
+++ server/settings.py
@@ -182,3 +182,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
 
 }
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+)
