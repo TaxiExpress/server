e0e27fbbcf13350971f068eed7fa54cbc232b13f
Laura Sainz de murieta <laura.sdml@gmail.com>
2014-01-24 19:36:08 +0100

    combo changes

--- web/static/js/combos.js
+++ web/static/js/combos.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
     //Llamada al servidor para recuperar los paises
     options = "";
-    alert('pasa1');
     $.ajax({
         type: "GET",
         url: "/getcountries/",
         success: function(result) {
-            alert('existen paises');
-            alert(result.length);
             if (result.length != 0){
                 options = "<option value='" + "0" + "'>" + "Seleccione país..." + "</option>";
             }
             for (i = 0, len = result.length; i < len; i++) {
-            	pais = result[i];
-            	alert(pais.code);
-                options += "<option value='" + pais.code + "'>" + pais.name + "</option>";
-                alert(pais.name);
+            	country = result[i];
+                options += "<option value='" + country.code + "'>" + country.name + "</option>";
             } 
             $('#country').html(options);
         },
@@ -23,55 +18,57 @@ $(document).ready(function() {
             alert("Please report this error: " + errorThrown + xhr.status + xhr.responseText);
         }
     });   
-    alert('pasa2');
 
-    // options = "<option value='" + "0" + "'>" + "Seleccione país..." + "</option>";
-    // options += "<option value='" + "108" + "'>" + "España" + "</option>";
-    // options += "<option value='" + "109" + "'>" + "Francia" + "</option>";
-    
-
-	$('.cbocountry').change(function() {
-		posicion=document.getElementById('country').options.selectedIndex; 
+    $('.cbocountry').change(function() {
+    	posicion=document.getElementById('country').options.selectedIndex; 
     	codCountry=document.getElementById('country').options[posicion].value;
     	//Llamada al servidor para recuperar las provincias del pais seleccionado
-    	if (codCountry == "108"){
-    		options = "<option value='" + "0" + "'>" + "Seleccione provincia..." + "</option>";
-    		options += "<option value='" + "48" + "'>" + "Bizkaia" + "</option>";
-    		options += "<option value='" + "49" + "'>" + "Zamora" + "</option>";
-    		$('#state').html(options);
-    	}
-    	else if(codCountry == "109"){
-    		$('#state').html("");
-    		$('#city').html("");
-    	}
-        
+    	$.ajax({
+            type: "GET",
+            url: "/getstates/",
+            success: function(result) {
+                if (result.length != 0){
+                    options = "<option value='" + "0" + "'>" + "Seleccione provincia..." + "</option>";
+                }
+                else{
+            	    $('#state').html("");
+    		    $('#city').html("")
+                }
+                for (i = 0, len = result.length; i < len; i++) {
+            	    state = result[i];
+                    options += "<option value='" + state.code + "'>" + state.name + "</option>";
+                } 
+                $('#state').html(options);
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                alert("Please report this error: " + errorThrown + xhr.status + xhr.responseText);
+            }
+        });   
     });
 
     $('.cbostate').change(function() {
     	posicion=document.getElementById('state').options.selectedIndex; 
         codState=document.getElementById('state').options[posicion].value;
         //Llamada al servidor para recuperar los municipio de la provincia seleccionada
-    	if (codState == "48"){
-    		options = "<option value='" + "0" + "'>" + "Seleccione Municipio..." + "</option>";
-    		options += "<option value='" + "013" + "'>" + "Barakaldo" + "</option>";
-    		options += "<option value='" + "016" + "'>" + "Berango" + "</option>";
-    		options += "<option value='" + "020" + "'>" + "Bilbao" + "</option>";
-    		options += "<option value='" + "089" + "'>" + "Urduliz" + "</option>";
-    		$('#city').html(options);
-    	}
-    	else if(codState == "49"){
-    		options = "<option value='" + "0" + "'>" + "Seleccione Municipio..." + "</option>";
-    		options += "<option value='" + "013" + "'>" + "Zamora1" + "</option>";
-    		options += "<option value='" + "016" + "'>" + "Zamora2" + "</option>";
-    		options += "<option value='" + "020" + "'>" + "Zamora3" + "</option>";
-    		options += "<option value='" + "089" + "'>" + "Zamora4" + "</option>";
-    		$('#city').html(options);
-    	}
+        $.ajax({
+            type: "GET",
+            url: "/getcities/",
+            success: function(result) {
+                if (result.length != 0){
+                    options = "<option value='" + "0" + "'>" + "Seleccione municipio..." + "</option>";
+                }
+                else{
+    		    $('#city').html("")
+                }
+                for (i = 0, len = result.length; i < len; i++) {
+            	    city = result[i];
+                    options += "<option value='" + city.code + "'>" + city.name + "</option>";
+                } 
+                $('#city').html(options);
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                alert("Please report this error: " + errorThrown + xhr.status + xhr.responseText);
+            }
+        });
     });
-
-    // $('.cbocity').change(function() {
-    // 	posicion=document.getElementById('city').options.selectedIndex; 
-    //     codCity=document.getElementById('city').options[posicion].value;
-    //     alert(codCity);
-    // });	
 });
