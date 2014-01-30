8f682f081274c2166c4e656c70a055ff77978d7f
gorkar12 <gorkar12@gmail.com>
2014-01-30 21:10:15 +0100

    Debug

--- taxiexpress/views.py
+++ taxiexpress/views.py
@@ -138,6 +138,7 @@ def registerUser(request):
 @api_view(['POST'])
 def getClosestTaxi(request):
     if 'latitude' in request.POST:
+        print request.POST
         pointclient = Point(float(request.POST['latitude']), float(request.POST['longitude']))
         try:
             customer = Customer.objects.get(email=request.POST['email'])
