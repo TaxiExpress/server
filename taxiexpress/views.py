531a8c958b3cee91d596f48d16e84f104fc50ebd
gorkar12 <gorkar12@gmail.com>
2014-01-30 20:55:36 +0100

    Added driver availability check on selected driver

--- taxiexpress/views.py
+++ taxiexpress/views.py
@@ -195,6 +195,8 @@ def getSelectedTaxi(request):
         if customer.sessionID != request.POST['sessionID']:
             return HttpResponse(status=status.HTTP_401_UNAUTHORIZED, content="Debes estar conectado para realizar esta acción")
         driver = Driver.objects.get(email=request.POST['driverEmail'])
+        if (driver.available == False) or (driver.car.isfree == False):
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST, content="Driver unavailable")
         travel = Travel(customer=customer, startpoint=pointclient, origin=request.POST['origin'])
         travel.save()
         valuation = 0
