8893c0ae2a2aa5ed7f9c96675cf3d90bb63c87bb
MikelDTB <mikeldeltio90@gmail.com>
2014-02-16 10:58:05 +0100

    Update views.py

--- taxiexpress/views.py
+++ taxiexpress/views.py
@@ -171,7 +171,8 @@ def getClosestTaxi(request):
         if (customer.positiveVotes+customer.negativeVotes) > 0:
             valuation = int(5*customer.positiveVotes/(customer.positiveVotes+customer.negativeVotes)) #Calculate customer valuation (1..5) to send it to close drivers
         #punto = request.POST['startpoint'][0] + "," + request.POST['startpoint'][1]
-        post_data = {"title" : 'Un cliente solicita sus servicios' , "message" : 801, "origin": request.POST['origin'], "startpoint": 'bb', "travelID": request.POST['travelID'], "email": request.POST['email'], "phone": request.POST['phone']}         post_data_ios = {"pushId": []} #Dictionary to notify IOS users
+        post_data = {"title" : 'Un cliente solicita sus servicios' , "message" : 801, "origin": request.POST['origin'], "startpoint": "bb", "travelID": request.POST['travelID'], "email": request.POST['email'], "phone": request.POST['phone']}     
+        post_data_ios = {"pushId": []} #Dictionary to notify IOS users
         post_data_ios.update(post_data) #Add base dictionary data
         post_data_android = {"pushId": []} #Dictionary to notify ANDROID users
         post_data_android.update(post_data)
@@ -223,7 +224,7 @@ def getSelectedTaxi(request):
             valuation = int(5*customer.positiveVotes/(customer.positiveVotes+customer.negativeVotes)) #Calculate customer valuation (1..5) to send it to close drivers
         #Dictionary to be sent to PUSH server
         #punto = request.POST['startpoint'][0] + "," + request.POST['startpoint'][1]
-        post_data = {"pushId": driver.pushID , "title" : 'Un cliente solicita sus servicios' , "message" : 802, "origin": request.POST['origin'], "startpoint": 'bbb', "travelID": request.POST['travelID'], "email": request.POST['email'], "phone": request.POST['phone']} 
+        post_data = {"pushId": driver.pushID , "title" : 'Un cliente solicita sus servicios' , "message" : 802, "origin": request.POST['origin'], "startpoint": "bbb", "travelID": request.POST['travelID'], "email": request.POST['email'], "phone": request.POST['phone']} 
         try:
             resp = requests.post(PUSH_URL+'/push', params=post_data) #Send notify dictionary to PUSH server
         except requests.ConnectionError: #If push server is offline, delete travel and return 503
