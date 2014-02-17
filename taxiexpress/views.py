01f1ae679240ac89a3d0f828e0a14ec7435b915a
David Lallana <davidlallana@gmail.com>
2014-02-17 14:27:37 +0100

    debug

--- taxiexpress/views.py
+++ taxiexpress/views.py
@@ -170,7 +170,7 @@ def getClosestTaxi(request):
         elif closestDrivers.count() > 5:
             closestDrivers = closestDrivers[:5] #If mone than 5 drivers are found, reduce the list to 5 items
         pushIDS = {}        
-        for i in range(0,closestDrivers.count()-1):
+        for i in range(1,closestDrivers.count()-1):
             pushIDS.append(closestDrivers[i].pushID)
         travel = Travel(customer=customer, startpoint=pointclient, origin=request.POST['origin'])
         travel.save()
